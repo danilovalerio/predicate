cea42afe0e905e3e1714ccd53a53f8349baba7bf
danilovalerio <danilo_vs@hotmail.com>
2018-11-05 19:26:08 -0200

    Base para o exemplo do uso de Predicate

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,27 @@
+package aplicacao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.Produto;
+
+/*
+ * Fazer um programa que, a partir de uma lista de produtos, 
+ * remova da lista somente aqueles cujo preço mínimo seja 100.
+ * 
+ */
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		List<Produto> list = new ArrayList<>();
+		list.add(new Produto("Tv", 900.00));
+		list.add(new Produto("Mouse", 50.00));
+		list.add(new Produto("Tablet", 350.50));
+		list.add(new Produto("HD Case", 80.90));
+		
+
+	}
+
+}
